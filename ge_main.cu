c814acfc7593b8576e15982ab5d11b82591d88a0
kritinsai <kritinsai1@gmail.com>
2017-11-19 00:07:11 +0530

    ge corrected errors

--- ge_main.cu
+++ ge_main.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
 #include <thrust/extrema.h>
 #include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
@@ -8,6 +9,8 @@
 #include <thrust/copy.h>
 #include "mat_utils.h"
 #include "matrix.h"
+
+#define P printf("line: %d\n", __LINE__);
 
 
 /*
@@ -67,6 +70,8 @@ void ge_cpu(float *a, float *re_a, int N, int M, float *x) {
 
 template <typename T>
 struct absolute {
+
+	__device__
 	void operator()(T &x) const {
 		if (x < 0) {
 			x = -x;
@@ -126,47 +131,83 @@ void ge_parallel(float *h_A, float *h_row_echelon_A, int N, int M, float *X) {
 
 	cudaDeviceSynchronize();
 
+	if (0) {
+		float h_A_t[M][N];
+		cudaMemcpy(&h_A_t[0][0], d_A_t, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+
+		for (int i=0; i<M; i++) {
+			for (int j=0; j<N; j++) {
+				printf("%f ", h_A_t[i][j]);
+			}
+			printf("\n");
+		}
+	}
+
 	thrust::device_ptr<float> th_A = thrust::device_pointer_cast(d_A);
 	thrust::device_ptr<float> th_A_t = thrust::device_pointer_cast(d_A_t);
+
 	thrust::device_ptr<float> th_x = thrust::device_pointer_cast(X);
 
 	thrust::device_vector<float> tmp(M);
+
 
 	int max_row;
 	for (int i=0; i<N; i++) {
 		// Search for maximum in the ith column
 
-		thrust::for_each(th_A_t+idx(i,i,N), th_A_t+idx(i+1,0,N), absolute<float>());
+		thrust::for_each(thrust::device, th_A_t+idx(i,i,N), th_A_t+idx(i+1,0,N), absolute<float>());
+		cudaDeviceSynchronize();
+
+		thrust::copy(th_A_t+idx(i,i,N), th_A_t+idx(i,0,N)+N, std::ostream_iterator<int>(std::cout, "\n"));
+
 		thrust::device_vector<float>::iterator iter = 
-			thrust::max_element(th_A_t+idx(i,i,N), th_A_t+idx(i+1,0,N));
+			thrust::max_element(thrust::device, th_A_t+idx(i,i,N), th_A_t+idx(i+1,0,N));
+		cudaDeviceSynchronize();
+
 		max_row = thrust::device_pointer_cast(&(iter[0]))-(th_A_t+idx(i,0,N));
+
 
 		// Swap ith row with the maximum row (for numerical stability)
 		thrust::copy(thrust::device, th_A+idx(i,0,M), th_A+idx(i+1,0,M), tmp.begin());
+		cudaDeviceSynchronize();
 		thrust::copy(thrust::device, th_A+idx(max_row,0,M), th_A+idx(max_row+1,0,M), th_A+idx(i,0,M));
+		cudaDeviceSynchronize();
 		thrust::copy(thrust::device, tmp.begin(), tmp.end(), th_A+idx(max_row,0,M));
+		cudaDeviceSynchronize();
 
+		// P;
 
 		// Zero all entries in the ith column below the ith row
 		find_ratio_and_zero<<<num_blocks(N-(i+1), 512), 512>>>(d_A, N, M, i, d_ratio);
+		cudaDeviceSynchronize();
+
+		// P;
 
 		dim3 threads(16, 16);
 		dim3 blocks(num_blocks(N-(i+1),16), num_blocks(M-(i+1),16));
 		reduce_submatrix<<<threads, blocks>>>(d_A, N, M, i, d_ratio);
 
+		// P;
+
 		cudaDeviceSynchronize();
 	}
+
+	// P;
 
 	if (h_row_echelon_A != NULL) {
 		cudaMemcpy(h_row_echelon_A, d_A, N*M*sizeof(float), cudaMemcpyDeviceToHost);
 	}
 
+	// P;
+
 	if (X != NULL) {
 		thrust::fill(thrust::device, th_x, th_x+N, 0);
+		cudaDeviceSynchronize();
 
 		for (int i=N-1; i>=0; i--) {
 			X[i] = th_A[idx(i,N,M)]/th_A[idx(i,i,M)];
 			reduce_rhs<<<num_blocks(i,512), 512>>>(d_A, N, M, i, X);
+			cudaDeviceSynchronize();
 		}
 	}
 
@@ -192,11 +233,20 @@ int main() {
 	}
 
 	if (1) {
-		ge_cpu((float *) A, (float *) row_echelon_A, N, M, X);
+		ge_cpu((float *) A, (float *) row_echelon_A, N, M, NULL);
 	}
 
-	if (0) {
-		ge_parallel((float *) A, (float *) row_echelon_A, N, M, NULL);
+	if (1) {
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<M; j++) {
+				printf("%f ", row_echelon_A[i][j]);
+			}
+			printf("\n");
+		}
+	}
+
+	if (1) {
+		ge_parallel((float *) A, (float *) row_echelon_A, N, M, X);
 		cudaDeviceSynchronize();
 	}
 
